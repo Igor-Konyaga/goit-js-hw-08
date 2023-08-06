1b8ab0389ba5a60c58936ecb918699e7b8659c08
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-06 22:18:21 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,32 +10,28 @@ formEl.addEventListener('input', throttle(handleFormInput, 500));
 
 const currentStorage = JSON.parse(localStorage.getItem(KEY_STORAGE));
 
+if (currentStorage) {
+  formEl.email.value = currentStorage.email || '';
+  formEl.message.value = currentStorage.message || '';
+}
 
 function handleFormInput(e) {
-	dataForm[e.target.name] = e.target.value.trim();
-	localStorage.setItem(KEY_STORAGE, JSON.stringify(dataForm));
-
-};
-
+  dataForm[e.target.name] = e.target.value.trim();
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(dataForm));
+}
 
 formEl.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(e) {
-	e.preventDefault();
-	
-	if (currentStorage) {
-	  formEl.email.value = currentStorage.email || '';
-	  formEl.message.value = currentStorage.message || '';
-	};
+  e.preventDefault();
 
   const emailValue = e.target.elements.email.value;
   const messageValue = e.target.elements.message.value;
 
   if (!emailValue || !messageValue) {
-	 alert('Не усі поля заповнені!!!')
-  }
-
-  console.log(currentStorage);
-  formEl.reset();
-  localStorage.removeItem(KEY_STORAGE);
+    alert('Не усі поля заповнені!!!');
+  } 
+    console.log(dataForm);
+    formEl.reset();
+    localStorage.removeItem(KEY_STORAGE);
 }
